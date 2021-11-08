7d01cb13303674689ebb456383e1ee7e7b4856c3
E-rar <erik.ramich@gmx.de>
2021-11-08 15:49:28 +0100

    pullrequest

--- expensee/src/App.js
+++ expensee/src/App.js
@@ -1,10 +1,44 @@
 import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Expensee</h1>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/">
+          
+        </Route>
+        <Route path="/">
+          
+        </Route>
+        <Route path="/">
+        
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ expensee/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss'
+import {Link} from 'react-router-dom'
+function Header (){
+    return(
+    <header>
+
+    </header>
+    )
+
+
+}
+export default Header;
