6c57a76914a27541ffe383fe923c9f7831f9f1c7
Jonas <ermert.jonas@yahoo.de>
2021-11-09 10:56:54 +0100

    resetPassword.js implementiert

--- expensee/src/components/login/resetPassword.js
+++ expensee/src/components/login/resetPassword.js
@@ -9,3 +9,19 @@ Version: 1.0
 import React, { useState } from 'react'
 import { emailCheck } from './emailCheck'
 
+export default function ResetPassword({  }) {
+    const [email, setEmail] = useState({ value: '', error: '' })
+    const [loading, setLoading] = useState(false)
+
+
+    const sendResetPasswordEmail = async () => {
+        const emailError = emailCheck(email.value)
+        if (emailError) {
+          setEmail({ ...email, error: emailError })
+          return
+        }
+        setLoading(true)
+        const response = await sendEmailWithPassword(email.value)
+        setLoading(false)
+      }
+}
