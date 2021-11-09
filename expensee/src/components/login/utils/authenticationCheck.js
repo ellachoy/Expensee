ff851e468add84e99d0e405749c0694be810807d
Jonas <ermert.jonas@yahoo.de>
2021-11-09 15:34:14 +0100

    authenticationCheck kommentiert

--- expensee/src/components/login/utils/authenticationCheck.js
+++ expensee/src/components/login/utils/authenticationCheck.js
@@ -7,6 +7,14 @@ Version: 1.0
 
 import firebase from 'firebase/app'
 
+
+/* 
+
+  In dieser Funktion wird überprüft, ob der Lgin erfolgreich war. Wenn ja, wird der Nutzer zu "Home" weitergeleitet. Wenn nicht
+  wird er zurück zu "Login geleitet"
+
+*/
+
 export default function authenticationCheck({ navigation }) {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
