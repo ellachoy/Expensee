faba33651d88647cb2a56466ccb748e6dcd547f6
Jonas <ermert.jonas@yahoo.de>
2021-11-09 11:19:16 +0100

    resetPassword, login, register, authenticatioCheck weiter bearbeitet

---
+++ expensee/src/components/login/login.js
@@ -0,0 +1,37 @@
+/*
+
+Autor: Jonas Ermert
+Version: 1.0
+
+*/
+
+import React, { useState } from 'react'
+import { loginUser } from './firebase_config'
+import { emailCheck } from './emailCheck'
+import { passwordCheck } from './passwordCheck'
+
+export default function Login({ navigation }) {
+    const [email, setEmail] = useState({ value: '', error: '' })
+    const [password, setPassword] = useState({ value: '', error: '' })
+    const [loading, setLoading] = useState()
+    const [error, setError] = useState()
+
+    const onLogin = async () => {
+        const emailError = emailCheck(email.value)
+        const passwordError = passwordCheck(password.value)
+        if (emailError || passwordError) {
+          setEmail({ ...email, error: emailError })
+          setPassword({ ...password, error: passwordError })
+          return
+        }
+        setLoading(true)
+        const response = await loginUser({
+          email: email.value,
+          password: password.value,
+        })
+        if (response.error) {
+          setError(response.error)
+        }
+        setLoading(false)
+      }
+}
