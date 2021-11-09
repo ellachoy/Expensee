54fda6367905fc40635b158c93bd3005bfc390b0
Ella Choy <egal21de@gmail.com>
2021-11-09 15:45:02 +0100

    footerv5

--- expensee/src/pages/Home/Home.js
+++ expensee/src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import './Home.scss';
-import Header from '../../components/header/Header'
+import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
