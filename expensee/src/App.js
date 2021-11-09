9a8ab6f5426e160615df7824afe2fd77b635110f
Ella Choy <egal21de@gmail.com>
2021-11-09 10:14:48 +0100

    footer_up

--- expensee/src/App.js
+++ expensee/src/App.js
@@ -1,14 +1,30 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
+
 import './App.scss';
 import Footer from './components/Footer/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <h1>Expensee</h1>
+       
+      </Router>
+      
     
+   <Footer/>
 
-     <Footer/>
     </div>
+     
   );
 }
 
--- expensee/src/components/Footer/Footer.js
+++ expensee/src/components/Footer/Footer.js
@@ -1,8 +1,37 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+
 import './Footer.scss';
+import wallet from '../../img/wallet.png'
+import homebtn from '../../img/home_button_active.png'
+import chart from '../../img/charts.png'
 
 const Footer = () => {
-    return (  
-        <div>footer</div>
+    return ( 
+       
+        <footer className="footer">
+            <div className="footer-container">
+                <ul>
+                    <li> <NavLink exact to="/Einnahme">
+                        <img src={wallet} className="wallet" alt="wallet" /> Einnahme
+                        </NavLink>
+                    </li>
+                    <li> <NavLink exact to="/">
+                         <img src={homebtn} className="homebtn" alt="home" />Home
+                         </NavLink>
+                    </li>
+                    <li> <NavLink exact to="/Static"> 
+                         <img src={chart} className="charts" alt="" />
+                         </NavLink>
+                    </li>
+                </ul>
+            </div>
+            
+            </footer>
+
+        
     );
 }
  
