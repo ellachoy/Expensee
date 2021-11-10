f0fd71a5e73426d9b73bd2aa9ea0a32355833ac0
Jonas <ermert.jonas@yahoo.de>
2021-11-10 05:23:50 +0100

    Updates in firebase_config und login

--- expensee/src/components/login/login.js
+++ expensee/src/components/login/login.js
@@ -14,7 +14,6 @@ import { passwordCheck } from './utils/passwordCheck'
 export default function Login({ navigation }) {
     const [email, setEmail] = useState({ value: '', error: '' })
     const [password, setPassword] = useState({ value: '', error: '' })
-    const [loading, setLoading] = useState()
     const [error, setError] = useState()
 
     const onLogin = async () => {
@@ -45,9 +44,9 @@ const Login = () => {
         <div className="login-container">
           <form className="login-form">
             <label htmlFor="email">Bitte gib deine Email Adresse ein:</label>
-            <input type="text" placeholder="Email Adresse eingeben..."></input>
+              <input type="text" placeholder="Email Adresse eingeben..."></input>
             <label htmlFor="password">Bitte gib dein Passwort ein:</label>
-            <input type="password"></input>
+              <input type="password"></input>
           </form>
           <button type="submit" className="login-button">Einloggen</button>
           <p>Noch nicht registriert?</p><a href="./register.js">Registrieren</a>
