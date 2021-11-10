a11e40e0d59c308604fab441ee4d00e3577d862a
Jonas <ermert.jonas@yahoo.de>
2021-11-10 15:17:44 +0100

    nächster Login versuch

--- expensee/src/components/login/login.js
+++ expensee/src/components/login/login.js
@@ -12,6 +12,9 @@ import { passwordCheck } from './utils/passwordCheck'
 // import { authenticationCheck} from './utils/authenticationCheck'
 // import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom'
+import { useState } from 'react';
+// import { useLogin } from '../hooks/useLogin';
 
 // Style
 import {Style} from './login.scss'
@@ -122,5 +125,43 @@ const Login = () => {
   );
   
 }
+
+/*
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { error, login } = useLogin()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email, password)
+        login(email, password)
+    }
+    return (
+        <div id="login">
+            <h1>Log In</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label>Email:</label>
+                <input type="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+
+                <label>Password:</label>
+                <input type="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+
+                <input type="submit" value="Log In" />
+            </form>
+            <p>You don't have an Account, please <Link to="/signup">sign up</Link></p>
+            {error && <p>{error}</p>}
+        </div>
+    );
+}
+
+*/
+
 
 export default Login;
