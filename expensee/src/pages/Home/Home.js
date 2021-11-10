68057e5154801634a99c8496895ccf3b95c8055b
ella <egal21de@gmail.com>
2021-11-10 11:01:28 +0100

    readypull

--- expensee/src/pages/Home/Home.js
+++ expensee/src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import './Home.scss';
-import Header from '.../components/Header/Header.js'
+import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
