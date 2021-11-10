f0347a23b852240a7c99aa4e937c94840b9bc917
E-rar <erik.ramich@gmx.de>
2021-11-10 13:56:03 +0100

    pullreq

--- expensee/src/pages/Charts/Charts.js
+++ expensee/src/pages/Charts/Charts.js
@@ -3,10 +3,12 @@ import './Charts.scss';
 import Header from '../../components/Header/Header'
 import Collaps from '../../components/Collaps/Collaps'
 
+
 const Charts = () => {
     return (  
         <div>Charts
         <Header title="Statistik"/>
+        
         <Collaps/>
         <Footer/>
         </div>
