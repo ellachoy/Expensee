631e42fbaf80d4d856b85b2b72e3783a1505f101
E-rar <erik.ramich@gmx.de>
2021-11-11 15:41:00 +0100

    transaktionen

--- expensee/src/pages/Home/Home.js
+++ expensee/src/pages/Home/Home.js
@@ -1,19 +1,14 @@
 import './Home.scss';
 import Header from '../../components/Header/Header.js';
 import Footer from '../../components/Footer/Footer';
-import Button from '../../components/Button/Button';
+import TransaktionContainer from '../../components/Transaktion/TransaktionContainer'
 
 
 const Home = () => {
     return (  
         <div>
             <Header title="Übersicht"/>
-            
-            
-            
-        
-            
-    
+            <TransaktionContainer/>
             <Footer/>
         </div>
     );
