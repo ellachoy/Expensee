f048c69693eac024c4444242b9a5f65861146bef
Jonas <ermert.jonas@yahoo.de>
2021-11-11 10:41:43 +0100

    Header umbenannt

--- expensee/src/components/login/utils/authenticationCheck.js
+++ expensee/src/components/login/utils/authenticationCheck.js
@@ -5,12 +5,13 @@ Version: 1.0
 
 */
 
+import React from 'react'
 import firebase from 'firebase/app'
 
 
 /* 
 
-  In dieser Funktion wird überprüft, ob der Lgin erfolgreich war. Wenn ja, wird der Nutzer zu "Home" weitergeleitet. Wenn nicht
+  In dieser Funktion wird überprüft, ob der Login erfolgreich war. Wenn ja, wird der Nutzer zu "Home" weitergeleitet. Wenn nicht
   wird er zurück zu "Login" geleitet
 
 */
