570de4b9a222bed3c0c7bc41f45653d4f13988e9
Babera91 <ncis@hotmail.de>
2021-11-11 09:16:50 +0100

    Uppercase

--- expensee/src/pages/Home/Home.js
+++ expensee/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import './Home.scss';
 
-import Header from '../../components/Header/Header'
+import Header from '../../components/Header/Header';
 
 import Footer from '../../components/Footer/Footer';
 import Button from '../../components/Button/Button';
