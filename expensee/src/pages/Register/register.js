09bef8a46ebea104f7e6f22f0e8a6cf7a569358e
codaersimon <codaersimon@gmail.com>
2021-11-17 15:09:16 +0000

    pullreq

--- expensee/src/pages/Register/register.js
+++ expensee/src/pages/Register/register.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext'
 import shapeImg from '../../img/shape.png'
+import backImg from '../../img/background.png'
 
 const Register = () => {
     const [email, setEmail] = useState("")
@@ -26,24 +27,30 @@ const Register = () => {
             <h1 className="registerh1">Expensee</h1>
             <h3 className="registerh3">Account erstellen</h3>
             <form className="registerMail" onSubmit={handleSubmit}>
-                <input 
-                    type="email" 
-                    name="email" 
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <br />
-                <input 
-                    type="password" 
-                    name="password" 
-                    placeholder="Passwort"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                /><br />
+                <div className="conti">
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <br />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Passwort"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    /><br />
+                </div>
                 <button className="signUpB" type="submit">Sign Up</button>
             </form>
+            <div className="backGcont">
+                <img className="backG" src={backImg} alt="" />
+            </div>
         </div>
+
     );
 }
 
